9842caea1a8643f29f138683b3617f5cd62e4b60
kneeraazon01 <kneeraazon@gmail.com>
2023-06-17 02:15:10 +0545

    add transcription  and summary endpoints

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c585677008f3add8"; }
+/******/ 		__webpack_require__.h = function() { return "7c5eb49b1ef34243"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
